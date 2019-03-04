ea5c0e0fee935a07dfcd5daa963299417df92607
yessinekebir <yessine.kebir@gmail.com>
2019-03-04 20:11:08 +0100

    Port changed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var users = require('./routes/users');
 
 app.use('/admin', admin);
 app.use('/users', users);
+var port = process.env.PORT || 8080;
 
-app.listen(3001);
+app.listen(port);
 module.exports = app;
